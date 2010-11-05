83062bfc6c926c5c13e7fda068e88e8fe94330d3
mix3 <himachocost333@hotmail.co.jp>
2010-11-05 14:16:42 +0900

    死活メール送信メソッド追加

--- lib/CloudForecast/Component/AlertMail.pm
+++ lib/CloudForecast/Component/AlertMail.pm
@@ -3,36 +3,91 @@ package CloudForecast::Component::AlertMail;
 use CloudForecast::Component -connector;
 
 use Path::Class;
-use Cache::Memory;
+use Cache::File;
 use Mail::Sendmail;
 use Encode;
 use utf8;
 
-sub update {
+sub alert_die_update {
     my ( $self, $subject, $alert ) = @_;
 
-    my $c = Cache::Memory->new(
+    my $c = Cache::File->new(
+        cache_root      => '/tmp/alert_cache',
         namespace       => 'AlertMailCache',
         default_expires => '600 sec'
     ) or die "$!";
 
-    my $is_alert = $c->get($alert) || 0;
+    my $is_alert = $c->get($subject) || 0;
+    my $next = $alert ? $is_alert + $alert : 0;
+    $c->set($subject, $next);
+
+    return $is_alert;
+}
+
+sub alert_die_send {
+    my ( $self, $alert ) = @_;
+    
+    my @pkg = split(/::/, ref($self));
+    my $subject = $pkg[$#pkg] . $self->str_subject("die");
+    my $is_alert = $self->alert_die_update($subject, $alert);
+
+    $alert    = $is_alert > 4 ? $alert : 0;
+    $is_alert = $is_alert > 5 ?      1 : 0;
+    return if($alert == $is_alert);
+
+    CloudForecast::Log->debug("start send alert die mail");
+
+    my $body = $alert ? "もしかして：　死んでる" : "もしかして：　復活";
+    $self->send_mail($subject, $body);
+}
+
+sub alert_update {
+    my ( $self, $subject, $alert ) = @_;
+
+    my $c = Cache::File->new(
+        cache_root      => '/tmp/alert_cache',
+        namespace       => 'AlertMailCache',
+        default_expires => '600 sec'
+    ) or die "$!";
+
+    my $is_alert = $c->get($subject) || 0;
     $c->set($subject, $alert);
 
     return $is_alert;
 }
 
-sub sub_send {
-    my $self = shift;
-    my ($subject, $alert) = @_;
+sub alert_sub_send {
+    my ($self, $subject, $alert) = @_;
 
-    my $is_alert = $self->update($subject, $alert);
+    my $is_alert = $self->alert_update($subject, $alert);
     return if($is_alert == $alert);
     
     CloudForecast::Log->debug("start send alert mail");
     
     my $body = $alert ? "閾値を超えました。ヤバいです。" : "閾値を下回りました。もう大丈夫です。";
-    
+    $self->send_mail($subject, $body);
+}
+
+sub alert_send {
+    my ($self, $alert) = @_;
+
+    while( my ($k, $v) = each(%$alert) ){
+        $self->alert_sub_send($k, $v);
+    }
+}
+
+sub str_subject {
+    my $self = shift;
+    my $suffix = shift || "";
+    my %info;
+    $info{ad} = $self->address;
+    $info{h}  = $self->hostname;
+    $info{as} = join ":", @{$self->args} if @{$self->args};
+    return ( keys %info ) ? join(",", map { "$_:$info{$_}" } keys %info) . " " : $suffix;
+}
+
+sub send_mail {
+    my ($self, $subject, $body) = @_;
     $subject = encode("MIME-Header-ISO_2022_JP", $subject);
     $body    = encode("iso-2022-jp", $body);
 
@@ -45,14 +100,6 @@ sub sub_send {
     );
 
     sendmail(%mail);
-}
-
-sub send {
-    my ($self, $alert) = @_;
-
-    while( my ($k, $v) = each(%$alert) ){
-        $self->sub_send($k, $v);
-    }
 }
 
 1;
